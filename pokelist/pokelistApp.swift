6f9304aa843380feb30f8b53b0be6803483a704a
wallrony <wrony.neves@gmail.com>
2022-11-08 09:51:01 -0300

    Initial Commit

---
+++ pokelist/pokelistApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokelistApp.swift
+//  pokelist
+//
+//  Created by Rony on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct pokelistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
